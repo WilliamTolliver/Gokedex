1d646cf9b87db01825bf49a60ea4de1346282ce7
Bludbathmcgrath <William_R_Tolliver@yahoo.com>
2016-07-20 06:13:08 -0400

    Added Google Maps PokemonLocator Activity. Updated Gradle build for Multidex application. Added Themes for AppCompat Activity to layouts.

--- app/src/main/java/wttechnologies/com/pokedextest/MainActivity.java
+++ app/src/main/java/wttechnologies/com/pokedextest/MainActivity.java
@@ -1,9 +1,13 @@
 package wttechnologies.com.pokedextest;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.GridView;
 
 import java.util.List;
@@ -29,6 +33,16 @@ public class MainActivity extends AppCompatActivity {
             // Create GridView and get list of Pokemon
             GridView gridView = (GridView) findViewById(R.id.grid_view);
             ConfigureViews.setGridView(this, gridView);
+
+            Button mapsButton = (Button) findViewById(R.id.maps_button);
+            Context ctx = this;
+            mapsButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent mapsIntent = new Intent(getBaseContext(), MapsActivity.class);
+                    startActivity(mapsIntent);
+                }
+            });
 
             //gridView.setBackground(getResources().getDrawable(R.drawable.teams2));
             List<Pokemon> pokemon = DatabaseUtil.setAttacks(this, DatabaseUtil.getPokemon(this, R.raw.list));
